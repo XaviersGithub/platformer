1268de44743264392373d2be4c1f893deefb11c3
Xavier Cowan <22cowanx@cistercian.org>
2021-11-16 11:17:56 -0600

    bg

--- core/src/cps/platformer/PlatformerMain.java
+++ core/src/cps/platformer/PlatformerMain.java
@@ -3,15 +3,17 @@ package cps.platformer;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class PlatformerMain extends Game {
 	public SpriteBatch batch;
 	Texture img;
-	
+	Rectangle mario;
 	@Override
 	public void create () {
+		mario = new Rectangle();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bg.png");
         setScreen(new MainMenuScreen(this));
 	}
 
--- desktop/src/cps/platformer/desktop/DesktopLauncher.java
+++ desktop/src/cps/platformer/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Your Title Here";
-        config.width = 800;
-        config.height = 480;
+        config.width = 256;
+        config.height = 224;
 		new LwjglApplication(new PlatformerMain(), config);
 	}
 }
