c2eab0a167104181f35538f08a992a5dd0d0b51e
XaviersGithub <90006106+XaviersGithub@users.noreply.github.com>
2021-11-16 10:47:39 -0600

    Initial commit

---
+++ core/src/cps/platformer/PlatformerMain.java
@@ -0,0 +1,28 @@
+package cps.platformer;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PlatformerMain extends Game {
+	public SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+        setScreen(new MainMenuScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/cps/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package cps.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cps.platformer.PlatformerMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Your Title Here";
+        config.width = 800;
+        config.height = 480;
+		new LwjglApplication(new PlatformerMain(), config);
+	}
+}
