62d5cb374f41a47e8598ecb5f0087a9863fc59da
Xavier Cowan <22cowanx@cistercian.org>
2021-11-18 10:35:43 -0600

    Partial merge of Xavier and main

---
+++ core/src/cps/platformer/Playerobj.java
@@ -0,0 +1,12 @@
+package cps.platformer;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Playerobj extends Rectangle {
+    public Vector2 velocity;
+
+    Playerobj () {
+        velocity = new Vector2();
+    }
+}
