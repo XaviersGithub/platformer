c60cfccca7672e7f2f2fd2184809eef0b53229ec
XaviersGithub <90006106+XaviersGithub@users.noreply.github.com>
2021-11-17 22:29:27 -0600

    Test changes

--- core/src/cps/platformer/PlatformerMain.java
+++ core/src/cps/platformer/PlatformerMain.java
@@ -1,15 +1,22 @@
 package cps.platformer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Interpolation.Elastic;
 
 public class PlatformerMain extends Game {
 	public SpriteBatch batch;
 	Texture img;
+	Playerobj player;
+	private float gravity = 10;
 	
 	@Override
 	public void create () {
+		player = new Playerobj();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
         setScreen(new MainMenuScreen(this));
@@ -17,7 +24,29 @@ public class PlatformerMain extends Game {
 
 	@Override
 	public void render () {
-		super.render();
+		batch.begin();
+		batch.draw(img, player.x, player.y);
+		batch.end();
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			player.velocity.x = -2;
+		}
+		else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			player.velocity.x = 2;
+		}
+		else {
+			player.velocity.x = 0;
+		}
+
+
+		player.velocity.y -= gravity * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+			player.velocity.y =3;
+		}
+		else if (player.y <= 0) {
+			player.velocity.y = 0;
+		}
+		player.x += player.velocity.x;
+		player.y += player.velocity.y;
 	}
 	
 	@Override
